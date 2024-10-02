86efff0b255f35fd8156b8794925ee420bbb7690
Kilyan Fossey <fosseykilyan@gmail.com>
2024-10-02 19:47:42 +0200

    fix: fixed index.tsx import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RocketInputController from './'
+import RocketInputController from './index.tsx'
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
